7360413dd536a3c02cfe807685628e8c4c6f7363
Atomm94 <atom.makaryan.94@gmail.com>
2020-12-07 17:07:31 +0400

    update

--- Api/Witness/index.js
+++ Api/Witness/index.js
@@ -176,7 +176,7 @@ const update = async (req,res) => {
         const witnessUpdate = await witnessModel.updateOne({_id:decodeToken.data.id, status: status.ACTIVE, disabled: false}, body);
         const witnessData = await witnessModel.findOne({_id: decodeToken.data.id});
         res.message = `This (${decodeToken.data.id}) witness updated successfully!`
-        return successHandler(res, witnessUpdate);
+        return successHandler(res, witnessData);
     } catch (err) {
         return errorHandler(res, err)
     }
